89b9ad74b9670003445aa7ed3650f684168cc092
Jakub Łaptaś <kontakt@kubens.pl>
2023-04-16 13:43:45 +0200

    draft added ttl for ICalendar

--- packages/icalendar/src/ICalendar.ts
+++ packages/icalendar/src/ICalendar.ts
@@ -1,26 +1,38 @@
 import { CalendarObject } from './CalendarObject'
+import { Duration } from './Duration'
 import { DEFAULT_PRODUCT_ID, formatProductIdentifier } from './ProductIdentifier'
 
-type CalendarProperties = {
+type ICalendarMethod = 'PUBLISH' | 'REQUEST' | 'REPLY' | 'ADD' | 'CANCEL' | 'REFRESH' | 'COUNTER' | 'DECLINECOUNTER'
+
+type ICalendarProperties = {
   CALSCALE: string
-  METHOD: string
+  METHOD: ICalendarMethod
   PRODID: string
   VERSION: string
+  'REFRESH-INTERVAL': Duration
+  'X-PUBLISHED-TTL': Duration
 }
 
-const schema: CalendarObject.Schema<keyof CalendarProperties> = {
+const schema: CalendarObject.Schema<keyof ICalendarProperties> = {
   CALSCALE: { required: false, once: true },
   METHOD: { required: false, once: true },
   PRODID: { required: true, once: true },
   VERSION: { required: true, once: true },
+  'REFRESH-INTERVAL': { required: false, once: true },
+  'X-PUBLISHED-TTL': { required: false, once: true },
 }
 
-export class ICalendar extends CalendarObject<CalendarProperties> {
+export class ICalendar extends CalendarObject<ICalendarProperties> {
   constructor(productId = DEFAULT_PRODUCT_ID) {
     super('VCALENDAR', schema)
 
     // Create base properties
     this.addProperty('VERSION', '2.0')
     this.addProperty('PRODID', formatProductIdentifier(productId))
+  }
+
+  setTTL(duration: Duration): void {
+    this.setProperty('REFRESH-INTERVAL', duration, { VALUE: 'DURATION' })
+    this.setProperty('X-PUBLISHED-TTL', duration)
   }
 }
